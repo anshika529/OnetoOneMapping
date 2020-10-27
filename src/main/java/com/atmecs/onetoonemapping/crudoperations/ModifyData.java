92db2a2200f2431fd58caf916c32e012ada094c2
Anshika_Negi <anshika.negi@atmecs.com>
2020-10-27 20:30:21 +0530

    Commit

--- src/main/java/com/atmecs/onetoonemapping/crudoperations/ModifyData.java
+++ src/main/java/com/atmecs/onetoonemapping/crudoperations/ModifyData.java
@@ -15,7 +15,25 @@ public class ModifyData {
 		Session session = HibernateUtil.currentSession();
 		Scanner sc = new Scanner(System.in);
 		try {
-			s
+			session.beginTransaction();
+			System.out.println("Enter id to modify record:");
+			Users user = (Users) session.get(Users.class, sc.nextInt());
+
+			if (user != null) {
+				System.out.println("Enter the new email to update");
+			//	user.setEmail(sc.next());
+				session.saveOrUpdate(user);
+
+				session.getTransaction().commit();
+				System.out.println("Record updated successfully..!!");
+			} else {
+				System.out.println("Record not found for given id, please enter a correct id");
+			}
+		} catch (SessionException e) {
+			System.out.println(e);
+		} finally {
+			HibernateUtil.closeSession();
+		}
 
 	}
 }
